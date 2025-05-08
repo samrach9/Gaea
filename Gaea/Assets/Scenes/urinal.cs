0ac51580331411ead50f41222ca605742f0c6cb2
ArranKooner <akooner@scu.edu>
2025-05-07 23:54:07 -0700

    rsource + urls!!!!! urinla

---
+++ Gaea/Assets/Scenes/urinal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urinal : MonoBehaviour
+{
+    // = "https://www.example.com";
+
+    public void OpenWebPage(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+}
